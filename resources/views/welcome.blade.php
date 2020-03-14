0213a23cc7a769439e3bc11261e8e23daa44df86
montolcsb <application.skyict.it@gmail.com>
2020-03-14 14:37:59 +0700

    edit bug1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </head>
+<title>Document</title>
+</head>
+
 <body>
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">หัวข้อ</th>
-      <th scope="col">คำตอบ</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    @foreach($values as $value)
-      <td>{{$value->id}}</td>
-      <td>{{$value->question}}</td>
-      <td>@{{$value->answer}}</td>
-      @endforeach
-    </tr>
-  </tbody>
-</table>
+    <table class="table">
+        <thead>
+            <tr>
+
+            </tr>
+        </thead>
+        <tbody>
+            @for($x = 0; $x <= count($values )-1; $x++) <tr>
+                <td>{{$values[$x][0]}}</td>
+                <td>{{$values[$x][1]}}</td>
+                <td>{{$values[$x][2]}}</td>
+                </tr>
+                @endfor
+        </tbody>
+    </table>
 </body>
+
 </html>
